e2ae4d3253bf1b9fefb15dab2401801de28398c7
dmytrot <ug.dotenko@gmail.com>
2024-04-13 12:58:42 +0300

    update

--- src/contacts/routes.py
+++ src/contacts/routes.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter, Depends
+from typing import Any
+
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
+from fastapi.responses import JSONResponse
 
 from db import get_db, ContactORM
 from contacts.models import Contact, ContactResponse
@@ -15,11 +18,14 @@ async def read(
     return [ContactResponse.from_orm(_) for _ in db.query(ContactORM).all()]
 
 
-@router.get("/{contact_id:int}")
+@router.get("/{contact_id:int}", response_model=ContactResponse)
 async def read_id(contact_id: int,
                   db: Session = Depends(get_db)
-                  ) -> ContactResponse:
+                  ) -> Any:
     res = db.get(ContactORM, contact_id)
+    if res is None:
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,
+                            content=f"Contact with id {contact_id} not found")
     return ContactResponse.from_orm(res)
 
 
--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI
 import uvicorn
+from fastapi import FastAPI
 
 from contacts.routes import router
 
