48bc5ac5d01b5a704d2a93d2367d9e7abd989e98
Dmytro-Tarasenko <ug.dotenko@gmail.com>
2024-05-07 17:20:57 +0300

    Test aujthentification service

--- tests/test_auth_service.py
+++ tests/test_auth_service.py
@@ -1,6 +1,8 @@
 import unittest
-from unittest.mock import MagicMock
+from datetime import datetime, timedelta, timezone
+
 from bcrypt import hashpw, gensalt
+from jose import jwt
 
 from src.db import get_db
 from src.auth.service import Authentication as auth_service
@@ -10,7 +12,7 @@ class TestAuthentication(unittest.TestCase):
     hashed_password = hashpw("password".encode(),
                              gensalt()).decode()
 
-    def test_verify_password_pass(self):
+    def test_verify_password_success(self):
         self.assertIs(True,
                       auth_service.verify_password(
                           auth_service,
@@ -25,6 +27,30 @@ class TestAuthentication(unittest.TestCase):
                           plain_password="secret",
                           hashed_password=self.hashed_password
                       ))
+
+    def test_create_refresh_token_success(self):
+        current_time = datetime.now(timezone.utc)
+        expiration = current_time + timedelta(minutes=15)
+        token = auth_service.create_refresh_token(
+            auth_service,
+            email="some@email.com",
+        )
+        check_payload = jwt.decode(
+            token=jwt.encode(
+                claims={'sub': 'some@email.com',
+                        'exp': expiration,
+                        'scope': 'refresh_token'},
+                key=auth_service.SECRET_512,
+                algorithm=auth_service.REFRESH_ALGORITHM
+            ),
+            key=auth_service.SECRET_512,
+            algorithms=[auth_service.REFRESH_ALGORITHM]
+        )
+        payload = jwt.decode(token=token,
+                             key=auth_service.SECRET_512,
+                             algorithms=[auth_service.REFRESH_ALGORITHM],
+                             )
+        self.assertDictEqual(payload, check_payload)
 
 
 if __name__ == "__main__":
